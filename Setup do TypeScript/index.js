cc3733e7431d94c92be03281d0bc0c761424e14d
Wanderson <121998974+wandersonst7@users.noreply.github.com>
2023-12-30 11:07:24 -0300

    Setup do TypeScript

---
+++ Setup do TypeScript/index.js
@@ -0,0 +1,10 @@
+var firstName = "Wanderson";
+var anotherName = 1;
+var x = true;
+// Tipagem
+function greeting(name) {
+    console.log("Olá, " + name);
+}
+greeting(firstName);
+// greeting(anotherName)
+// greeting(x)
---
+++ Setup do TypeScript/index.ts
@@ -0,0 +1,12 @@
+const firstName = "Wanderson";
+const anotherName = 1;
+const x = true;
+
+// Tipagem
+function greeting(name: string){
+    console.log("Olá, " + name)
+}
+
+greeting(firstName)
+// greeting(anotherName)
+// greeting(x)
